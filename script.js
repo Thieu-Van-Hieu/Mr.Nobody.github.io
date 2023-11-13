5a4a80531d1ab01327eb240bacb24edf32dcd73b
Mr.NoBody <102453266+Thieu-Van-Hieu@users.noreply.github.com>
2023-11-14 05:53:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function generateRandomFileName(length) {
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let randomFileName = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        randomFileName += characters.charAt(randomIndex);
+    }
+
+    return randomFileName;
+}
+function hackFile() {
+    document.getElementById('btn_hackfile').style.display = 'none'
+    document.getElementById('pb').style.display = 'block'
+    document.getElementById('consoleText').innerHTML += "<br>> Start hacking file...";
+    document.getElementById('consoleText').innerHTML += "<br>> Initiating file hack...";
+    document.getElementById('consoleText').innerHTML += "<br>> Running exploit code...";
+    document.getElementById('consoleText').innerHTML += "<br>> Accessing mainframe... ";
+
+    // Hiệu ứng nhiều file chạy tùm lum (nhanh hơn và khác nhau)
+    for (let i = 0; i < 15; i++) {
+        setTimeout(function () {
+            let fileType = Math.random() > 0.5 ? "exe" : "dll";
+            document.getElementById('consoleText').innerHTML += `<br>>` + generateRandomFileName(Math.floor(Math.random() * 30) + 10) + `.${fileType}...`;
+        }, i * 100);
+    }
+    setTimeout(function(){
+    document.getElementById('hack_fn').style.display = "block";
+    }, 4500)
+    // Sau một khoảng thời gian, hiển thị thông báo "Download File"
+    setTimeout(function () {
+        document.getElementById('downloadBtn').style.display = 'inline-block';
+        document.getElementById('loading').style.display = 'none';
+    }, 5000);
+}
+
+function downloadFile() {
+    document.getElementById('downloadBtn').style.display = 'none';
+    document.getElementById('loading').style.display = 'inline-block';
+    setTimeout(function () {
+        alert("File downloaded successfully!");
+        document.getElementById('loading').style.display = 'none';
+    }, 3000);
+}
+
+function startProgress() {
+    let progressBar = document.getElementById("pb");
+    let width = 0;
+
+    let interval = setInterval(() => {
+        if (width >= 100) {
+            clearInterval(interval);
+        } else {
+            width++;
+            progressBar.style.width = width + "%";
+            progressBar.innerHTML = width + "%";
+        }
+    }, 45);
+}
