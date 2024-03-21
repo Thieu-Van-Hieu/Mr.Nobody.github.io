6556214ae5b51f4d021d2876984d5e3d3c0732ba
Mr.NoBody <thieuefvanwhieues@gmail.com>
2024-03-21 16:54:48 +0700

    Quiz change

--- script.js
+++ script.js
@@ -18,12 +18,13 @@ function hackFile() {
     document.getElementById('consoleText').innerHTML += "<br>> Accessing mainframe... ";
 
     // Hiệu ứng nhiều file chạy tùm lum (nhanh hơn và khác nhau)
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 80; i++) {
         setTimeout(function () {
             let fileType = Math.random() > 0.5 ? "exe" : "dll";
-            document.getElementById('consoleText').innerHTML += `<br>>` + generateRandomFileName(Math.floor(Math.random() * 30) + 10) + `.${fileType}...`;
-        }, i * 100);
+            document.getElementById('consoleText').innerHTML = `<br>>` + generateRandomFileName(Math.floor(Math.random() * 30) + 10) + `.${fileType}...`;
+        }, i * 50);
     }
+
 }
 
 function downloadFile() {
@@ -49,11 +50,14 @@ function startProgress() {
         }
     }, 45);
     setTimeout(function(){
-    document.getElementById('hack_fn').style.display = "block";
-    }, 4500)
-    // Sau một khoảng thời gian, hiển thị thông báo "Download File"
+        document.getElementById('hack_fn').style.display = "block";
+        }, 4000)
     setTimeout(function () {
+        document.getElementById('pb').style.display = 'none';
         document.getElementById('downloadBtn').style.display = 'inline-block';
         document.getElementById('loading').style.display = 'none';
     }, 5000);
+    setTimeout(function(){
+        document.getElementById('hack_text').style.display = 'none';
+        }, 4000)
 }
